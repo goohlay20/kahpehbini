f9cfa7c998c2fd8d45ba462cb1c5ef7f1ff46b9c
Glay_I <goohlay@gmail.com>
2024-07-14 22:07:02 +0800

    fixed: backend, cors localhost3

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,14 @@ const app = express();
 const baseURL="/api/v1";
 
 db();
-app.use(cors({origin:["http://localhost:5173"],credentials:true}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  })
+);
 app.use(express.json());
 app.use(helmet());
 
